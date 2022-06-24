94dd86f1a80a87e5fe729b8db2490d8aa43a3e97
Lasse Diercks <github@lassediercks.de>
2022-06-24 13:16:47 +0200

    add modes

--- src/util.js
+++ src/util.js
@@ -22,6 +22,12 @@ export const scalesIndexes = {
   bluesminor: [0, 3, 5, 6, 7, 10],
   hungarianminor: [0, 2, 3, 6, 7, 8, 11],
   oriental: [0, 1, 4, 5, 6, 9, 10],
+  NeapolitanMinor: [0, 1, 3, 5, 7, 8, 11],
+  lydanmode: [0, 2, 4, 6, 7, 9, 11],
+  Mixolydianmode: [0, 2, 4, 5, 7, 9, 10],
+  ionianmode: [0, 2, 4, 5, 7, 9, 11],
+  Aeolianmode: [0, 2, 3, 5, 7, 8, 10],
+  Phrygianmode: [0, 1, 3, 5, 7, 8, 10],
 };
 
 export const chords = {
@@ -31,6 +37,7 @@ export const chords = {
   diminished7: [0, 3, 6, 9],
   augmented: [0, 4, 8],
   maj7: [0, 4, 7, 11],
+  7: [0, 4, 7, 10],
   sus2: [0, 5, 7],
   sus4: [0, 2, 7],
 };
