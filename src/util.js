41d17e9096490b328ffa4b2b585caa7ef7f27b58
Lasse Diercks <github@lassediercks.de>
2023-03-03 14:06:46 +0100

    new stuff

--- src/util.js
+++ src/util.js
@@ -14,8 +14,8 @@ export const notes = [
 ];
 
 export const scalesIndexes = {
-  major: [0, 2, 4, 5, 7, 9, 11],
-  minor: [0, 2, 3, 5, 7, 8, 10],
+  StandardMajor: [0, 2, 4, 5, 7, 9, 11],
+  Standardminor: [0, 2, 3, 5, 7, 8, 10],
   bluesmajor: [0, 3, 5, 6, 7, 9],
   pentatonicmajor: [0, 2, 4, 7, 9],
   pentatonicminor: [0, 3, 5, 7, 10],
@@ -37,6 +37,7 @@ export const chords = {
   diminished7: [0, 3, 6, 9],
   augmented: [0, 4, 8],
   maj7: [0, 4, 7, 11],
+  min7: [0, 3, 7, 10],
   7: [0, 4, 7, 10],
   sus2: [0, 5, 7],
   sus4: [0, 2, 7],
