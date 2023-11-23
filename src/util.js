92d54f9ce2ba526f25995326f1870f7cf13d8d8c
Lasse Diercks <github@lassediercks.de>
2023-11-23 23:04:12 +0100

    add harmonic minor, phyrgian and gregorian

--- src/util.js
+++ src/util.js
@@ -17,17 +17,21 @@ export const scalesIndexes = {
   StandardMajor: [0, 2, 4, 5, 7, 9, 11],
   Standardminor: [0, 2, 3, 5, 7, 8, 10],
   bluesmajor: [0, 3, 5, 6, 7, 9],
+  harmonicminor: [0, 2, 3, 5, 7, 8, 11],
   pentatonicmajor: [0, 2, 4, 7, 9],
   pentatonicminor: [0, 3, 5, 7, 10],
   bluesminor: [0, 3, 5, 6, 7, 10],
   hungarianminor: [0, 2, 3, 6, 7, 8, 11],
   oriental: [0, 1, 4, 5, 6, 9, 10],
   NeapolitanMinor: [0, 1, 3, 5, 7, 8, 11],
-  lydanmode: [0, 2, 4, 6, 7, 9, 11],
+  lydianmode: [0, 2, 4, 6, 7, 9, 11],
   Mixolydianmode: [0, 2, 4, 5, 7, 9, 10],
   ionianmode: [0, 2, 4, 5, 7, 9, 11],
   Aeolianmode: [0, 2, 3, 5, 7, 8, 10],
   Phrygianmode: [0, 1, 3, 5, 7, 8, 10],
+  // Enter the rest of the scales here
+  gregorian: [0, 2, 4, 6, 7, 9, 11],
+  irishScale: [0, 2, 3, 6, 7, 9, 10],
 };
 
 export const chords = {
